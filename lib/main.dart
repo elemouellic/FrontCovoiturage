83666a85350ef120561b61f37d078859536bce72
elemouellic <114568852+elemouellic@users.noreply.github.com>
2024-03-24 18:15:17 +0100

    implement home front after success login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:frontcovoiturage/pages/auth_page.dart';
+import 'package:frontcovoiturage/pages/home_page.dart';
+import 'package:frontcovoiturage/services/authentication_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authService = AuthenticationService();
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Covoiturage Greta de Vannes',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AuthPage(),
+      // Check if the user is authenticated
+      home: FutureBuilder<String?>(
+        future: authService.getToken(),
+        builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            if (snapshot.data == null) {
+              return const AuthPage();
+            } else {
+              return const HomePage();
+            }
+          }
+        },
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,156 @@
 // lib/pages/home_page.dart
 import 'package:flutter/material.dart';
+import 'package:frontcovoiturage/services/authentication_service.dart';
+import 'package:frontcovoiturage/pages/auth_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+// Importez vos widgets ici
+// import 'package:frontcovoiturage/pages/trajets_page.dart';
+// import 'package:frontcovoiturage/pages/recherche_trajet_page.dart';
+// import 'package:frontcovoiturage/pages/vos_trajets_page.dart';
+// import 'package:frontcovoiturage/pages/publier_trajet_page.dart';
+// import 'package:frontcovoiturage/pages/mon_compte_page.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+Widget currentWidget = const Center(
+  child: SizedBox(
+    width: 250.0,
+    height: 100.0,
+    child: Center(
+      child: Text(
+        'Bienvenue sur l\'application de covoiturage du Greta de Vannes',
+        softWrap: true,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+        )
+      ),
+    ),
+  ),
+);
+  // Create an instance of the AuthenticationService
+  final authService = AuthenticationService();
+  // Attribute to store the username
+  String ?username = "...";
+
+  @override
+  void initState() {
+    super.initState();
+   loadUser();
+  }
+
+
+  Future<void> loadUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt('userId');
+    if (userId != null) {
+      final user = await authService.getPersonne(userId);
+      if (user != null) {
+        setState(() {
+          username = user['firstname'];
+        });
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: username == '...'
+            ? const CircularProgressIndicator()
+            : Text("Bienvenue $username"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await authService.logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const AuthPage()),
+              );
+            },
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Welcome to the home page!'),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.bus_alert),
+              title: const Text('Liste des trajets'),
+              onTap: () {
+                setState(() {
+                  currentWidget = const Center(child: Text('Liste des trajets'));
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.directions_car),
+              title: const Text('Rechercher un trajet'),
+              onTap: () {
+                setState(() {
+                  currentWidget = const Center(child: Text('Rechercher un trajet'));
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.list),
+              title: const Text('Vos trajets'),
+              onTap: () {
+                setState(() {
+                  currentWidget = const Center(child: Text('Vos trajets'));
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.list_alt_sharp),
+              title: const Text('Publier un trajet'),
+              onTap: () {
+                setState(() {
+                  currentWidget = const Center(child: Text('Publier un trajet'));
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Mon compte'),
+              onTap: () {
+                setState(() {
+                  currentWidget = const Center(child: Text('Mon compte'));
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
+      body: currentWidget,
     );
   }
 }
