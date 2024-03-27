46347e8f25f0c503033e2a9bb9ec87a5a0bc5110
elemouellic <114568852+elemouellic@users.noreply.github.com>
2024-03-27 17:17:23 +0100

    implement home__search-trip page

--- lib/pages/home__search_trip__trip_detail.dart
+++ lib/pages/home__search_trip__trip_detail.dart
@@ -14,41 +14,60 @@ class TripDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxWidth: 350, maxHeight: 400),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'De : ${trip['start_city']}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              'À : ${trip['arrive_city']}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              'Date : ${trip['traveldate']}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              'Conducteur : ${trip['driver_name']}',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              'Distance : ${trip['kmdistance']} kms',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await authService.insertInscription(trip['id'], user['id']);
-              },
-              child: Text('Réserver'),
-            ),
-          ],
+    return Scaffold(
+      body: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 350, maxHeight: 400),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'De : ${trip['start_city']}',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'À : ${trip['arrive_city']}',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'Date : ${trip['traveldate']}',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'Conducteur : ${trip['driver_name']}',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'Distance : ${trip['kmdistance']} kms',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  http.Response response = await authService.insertInscription(
+                      trip['id'], user['id']);
+                  var jsonResponse = jsonDecode(response.body);
+                  if (response.statusCode == 201) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                            'Réservation effectuée avec succès !'),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                            '${jsonResponse['message']}'),
+                      ),
+                    );
+                  }
+                },
+                child: Text('Réserver'),
+              ),
+            ],
+          ),
         ),
       ),
     );
